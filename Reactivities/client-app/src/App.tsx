5b0b696c929d8864640ae9750c7170e95d36a7c8
Tabish Sheikh <141209382+tabishcodes@users.noreply.github.com>
2024-01-18 18:23:27 +0530

    Featching data from api, some policies & Us of semantic ui

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
 
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
+
   return (
-    <h1>Social Media App</h1>
+    <div>
+      <Header as='h2' icon='users' content='Social Media App' />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
   )
 }
 
